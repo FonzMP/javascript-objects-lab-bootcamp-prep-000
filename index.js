8cc82bd6b5f7259f4a4ec4a5c019fbed51bfd485
Alexander Keith Wilson <fonzmp@gmail.com>
2018-04-16 08:04:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,6 +9,5 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
 }
 
 function deleteFromObjectByKey(object, key) {
-  var newObject = delete object[key];
-  return newObject;
+  
 }
